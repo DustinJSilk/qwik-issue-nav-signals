cc47027bea2a930b4210752ea5f05211907ba902
Dustin Silk <dustin@staze.com>
2023-06-12 10:23:22 +0100

    Add useTask$ issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useComputed$, useVisibleTask$ } from "@builder.io/qwik";
+import { component$, useComputed$, useTask$ } from "@builder.io/qwik";
 import { Link, useLocation } from "@builder.io/qwik-city";
 
 export default component$(() => {
@@ -6,15 +6,30 @@ export default component$(() => {
 
   const pathname = useComputed$(() => url.pathname);
 
-  useVisibleTask$(({ track, cleanup }) => {
+  // A task the just tracks the URL
+  useTask$(({ track, cleanup }) => {
     track(() => pathname.value);
 
-    // This should only run on page load for path '/'
-    console.log("Running task for path: ", pathname.value);
+    // This should run once on the server when the task is created
+    console.log("Running task A for path: ", pathname.value);
 
-    // This should only when leaving the page
-    cleanup(() => console.log("Cleaning up task for path", pathname.value));
+    // This should run once on the server when the task is cleaned up
+    cleanup(() => console.log("Cleaning up task A for path", pathname.value));
   });
+
+  // A task the tracks the URL but is run on the client at least once before navigating away
+  useTask$(
+    ({ track, cleanup }) => {
+      track(() => pathname.value);
+
+      // This should run once on the server then once in the browser
+      console.log("Running task B for path: ", pathname.value);
+
+      // This should run once on the server then once in the browser when navigating away
+      cleanup(() => console.log("Cleaning up task B for path", pathname.value));
+    },
+    { eagerness: "load" }
+  );
 
   return (
     <>
